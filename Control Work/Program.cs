b82623d08dc30a12ccded49bfcb6990f91750682
Bogdan Zanozdra <bz300788@gmail.com>
2022-08-09 23:23:55 +0300

    добавил программный код

--- Control Work/Program.cs
+++ Control Work/Program.cs
@@ -1,1 +1,10 @@
 ﻿
+string [] array = {"Hello", "Hi", "CSharp", "GB", "555"};
+string [] resultArray = new string [array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    resultArray[i] = array[i];
+
+}
+Console.Write($"[{String.Join(", ", array)}] -> [{String.Join(", ", resultArray)}]");
